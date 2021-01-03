de8d79c44221f08a29887f4c380436a0c7ae2bcc
BabluKD <43620755+BabluKD@users.noreply.github.com>
2021-01-03 21:57:26 +0530

    Update skills.component.jsx

--- src/pages/skills/skills.component.jsx
+++ src/pages/skills/skills.component.jsx
@@ -14,7 +14,7 @@ const Skills = () => {
   return (
     <div className="pt-3 pb-3" id="skills">
       <h1 className="text-center font-details-b pb-4">TECH SKILLS</h1>
-      <CardDeck className="justify-content-around" style={{ marginRight: "15px" }}>
+      <CardDeck className="justify-content-around">
         <Row className="d-flex justify-content-around">
           {/* Frontend */}
           <Col md={4}>
