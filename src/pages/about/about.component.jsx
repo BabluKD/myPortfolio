5468d94fd524b0d3088524891f8c2518cf545ea9
BabluKD <43620755+BabluKD@users.noreply.github.com>
2021-01-03 21:50:55 +0530

    Update about.component.jsx

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -14,7 +14,7 @@ const About = () => {
             <Container>
                 <Row className="pt-3 pb-5 align-items-center">
                     <Col xs={12} md={6}>
-                        <Row className="justify-content-center mb-2 ml-2 ml-md-0">
+                        <Row className="justify-content-center mb-2">
                             <Image className="profile justify-content-center" alt="Profile pic" src={Profile} thumbnail fluid />
                         </Row>
                     </Col>
